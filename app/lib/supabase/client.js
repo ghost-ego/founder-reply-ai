795676df04326b4e5213b2e28662bbb77c029c91
ghost-ego <tahsinjilan27@gmail.com>
2026-08-21 09:32:43 +0600

    Update client.js

--- app/lib/supabase/client.js
+++ app/lib/supabase/client.js
@@ -1,7 +1,7 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from "@supabase/supabase-js";
 
-export function createClient() {
-  return createBrowserClient(
+export function getSupabaseClient() {
+  return createClient(
     "https://lfrfcqfkaiiqpykaluqw.supabase.co",
     "sb_publishable_g5_j08qHLnyaEto_DBSKtA_GUjMN33W"
   );
